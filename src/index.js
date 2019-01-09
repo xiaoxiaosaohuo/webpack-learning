0c0808f1f6405e4d51a5885eac612cdbd48b6fe9
jinxin479 <jinxin479@126.com>
2019-01-09 09:53:58 +0800

    d

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 import test, { aaa } from './test';
 console.log(test, aaa);
-=======
-import './index.css';
-
-import test, { a } from './test';
-console.log(test, a);
->>>>>>> 182c6e180a6477cce32676f7ec4db1d727a25f94
 // import("./a").then((res)=>{
 //     let a = test+res
 //     console.log(a)
