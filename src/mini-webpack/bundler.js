d5e2b7ae434e590adb316a8137b0302f94863255
jinxin479 <jinxin479@126.com>
2019-03-18 09:58:35 +0800

    miniwebpack

---
+++ src/mini-webpack/bundler.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const path = require('path');
+const babylon  =require('babylon');
+const tranverse = require('babel-traverse').default;
+const {transformFromAst} = require('babel-core');
+let ID = 0;
+function createAsset(filename){
+    const content = fs.readFileSync(filename,'utf-8');
+    const ast = babylon.parse(content,{
+        sourceType:'module'
+    });
+    const dependencies = [];
+    tranverse(ast,{
+        ImportDeclaration:({node})=>{
+            dependencies.push(node.source.value);
+        }
+    });
+    const id = ID++;
+    const {code} = transformFromAst(ast, null, {
+        presets: ['env'],
+      });
+    return{
+        id,
+        filename,
+        dependencies,
+        code
+    }
+}
+
+
+
+function createGraph(entry){
+    const mainAsset = createAsset(entry);
+    const queue = [mainAsset];
+    for(const asset of queue){
+        asset.mapping = {};
+        const dirname = path.dirname(asset.filename);
+        asset.dependencies.forEach(relativePath=>{
+            const absolutePath = path.join(dirname, relativePath);
+            const child = createAsset(absolutePath);
+            asset.mapping[relativePath] = child.id;
+            queue.push(child);
+        })
+    }
+    return queue
+}
+function bundle (graph){
+    let modules = '';
+    graph.forEach(mod=>{
+        modules += `${mod.id}: [
+            function (require, module, exports) {
+              ${mod.code}
+            },
+            ${JSON.stringify(mod.mapping)},
+          ],`;
+    });
+    const result = `
+    (function(modules){
+        function require(id) {
+            const [fn, mapping] = modules[id];
+            function localRequire(name) {
+              return require(mapping[name]);
+            }
+            const module = { exports : {} };
+            fn(localRequire, module, module.exports);
+            console.log(module.exports)
+            return module.exports;
+          }
+          require(0);
+    })({${modules}})
+    `;
+    return result;
+}
+const graph = createGraph('./src/entry.js')
+const bundleJS = bundle(graph)
+console.log(bundleJS)
