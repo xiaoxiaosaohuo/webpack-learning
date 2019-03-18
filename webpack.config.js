d3eac5b16e0a18195d8841465fdd17280e5abc3a
jinxin479 <jinxin479@126.com>
2019-03-18 18:57:11 +0800

    修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ManifestPlugin = require("./src/manifestPlugin");
+// const ManifestPlugin = require("./src/manifestPlugin");
 debugger;
 module.exports = {
   mode: "development",
@@ -20,27 +20,27 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
-    new ManifestPlugin()
+    // new MiniCssExtractPlugin({
+    //   // Options similar to the same options in webpackOptions.output
+    //   // both options are optional
+    //   filename: "[name].css",
+    //   chunkFilename: "[id].css"
+    // }),
+    // new ManifestPlugin()
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              // you can specify a publicPath here
-              // by default it use publicPath in webpackOptions.output
-              publicPath: "../"
-            }
-          },
+        //   {
+        //     loader: MiniCssExtractPlugin.loader,
+        //     options: {
+        //       // you can specify a publicPath here
+        //       // by default it use publicPath in webpackOptions.output
+        //       publicPath: "../"
+        //     }
+        //   },
           "css-loader"
         ]
       }
